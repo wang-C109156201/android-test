fa4316d5b6eb101f609684d5e5a6730b60ce770f
C109156201 <C109156201@nkust.edu.tw>
2022-02-23 00:05:14 +0800

    wk1

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>姓名:王禾琪</Text>
+      <Text>學號:110919047</Text>
       <StatusBar style="auto" />
     </View>
   );
